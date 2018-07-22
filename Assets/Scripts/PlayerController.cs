a28ce619ed20205da7a556634116a60284bd9dfe
Czeide Avanzado <czeideavanzado@gmail.com>
2018-07-22 23:31:10 +0800

    Added sprites and player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D rigidbody;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+
+	private Collider2D collider;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+
+		collider = GetComponent<Collider2D>();
+
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.IsTouchingLayers(collider, whatIsGround);
+
+		rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space) && grounded) {
+			rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+		}
+
+		animator.SetFloat ("Speed", rigidbody.velocity.x);
+		animator.SetBool ("Grounded", grounded);
+	}
+}
