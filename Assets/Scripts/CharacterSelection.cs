3b5e2725e0d9cbdecad9eb9f68b362548efa29e0
Daniel L <daniel.lachica82@gmail.com>
2018-08-08 00:18:23 +0800

    Add character selection script

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour {
+
+	// Use this for initialization
+	// void Start () {
+		
+	// }
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+	
+	public string playGameLevel;
+	public string mainMenuLevel;
+
+	public void PlayGame() {
+		Application.LoadLevel(playGameLevel);
+	}
+
+	public void MainMenu() {
+		Application.LoadLevel(mainMenuLevel);
+	}
+}
